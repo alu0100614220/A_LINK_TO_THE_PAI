9f7f6896419f0af9799cf5cb42bec7bc9c3ad2c6
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-05-19 12:53:49 +0100

    Creando tests

---
+++ src/TableroTest.java
@@ -0,0 +1,14 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class TableroTest {
+
+	Tablero tableroTest = new Tablero();
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
