bf39f41e15a481a06dc3b47995e1d9c4f9848829
Sergio Rodríguez <alu0100699968@ull.edu.es>
2015-05-19 23:55:42 +0100

    Eliminando líneas comentadas de la interfaz Componentes

--- src/Componentes.java
+++ src/Componentes.java
@@ -1,16 +1,19 @@
 public interface Componentes {
 	// Resolución de la pantalla
-	// int ALTO = (int) Toolkit.getDefaultToolkit().getScreenSize().getHeight();
-	// int ANCHO = (int) Toolkit.getDefaultToolkit().getScreenSize().getWidth();
 	int ALTO = 700;
 	int ANCHO = 700;
 
-	// Estados de una casilla
+	/**
+	 * Posibles estados de una casilla.
+	 */
 	enum Estado {
 		Vacia, Obstaculo, Corazon, Enemigo, Puerta, Heroe, Cofre, Llave, Espada, Escudo
 	}
 
-	// Orientación de los personajes
+	
+	/**
+	 * Diferentes orientaciones.
+	 */
 	enum Orientacion {
 		Norte, Este, Sur, Oeste, AtacandoNorte, AtacandoEste, AtacandoSur, AtacandoOeste
 	}
